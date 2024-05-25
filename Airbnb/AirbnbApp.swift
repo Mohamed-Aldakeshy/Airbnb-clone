714a1095b974e5b931b0701ed1add8e57e7b74e4
Mohamed-Aldakeshy <hassounaaldakeshi88@gmail.com>
2024-05-25 11:26:01 +0200

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Mohamed Aldakeshy on 25/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
